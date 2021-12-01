f53401b74127c6cacbe5f02c5594b3da2c25607d
Rik van Toor <rik.van.toor@gmail.com>
2021-12-01 07:57:42 +0100

    Day 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,25 +1,42 @@
+use nom::character::complete::{i32, newline};
+use nom::multi::separated_list0;
 use nom::IResult;
 
 use crate::days::Day;
 
 pub struct Day01;
 
+fn helper(input: &[i32]) -> i32 {
+  let mut counter: i32 = 0;
+  for i in 1..input.len() {
+    if input[i] > input[i - 1] {
+      counter += 1;
+    }
+  }
+  counter
+}
+
 impl Day for Day01 {
-  type Input = String;
+  type Input = Vec<i32>;
 
-  fn parse(_input: &str) -> IResult<&str, Self::Input> {
-    unimplemented!("parser")
+  fn parse(input: &str) -> IResult<&str, Self::Input> {
+    separated_list0(newline, i32)(input)
   }
 
-  type Output1 = String;
+  type Output1 = i32;
 
-  fn part_1(_input: &Self::Input) -> Self::Output1 {
-    unimplemented!("part_1")
+  fn part_1(input: &Self::Input) -> Self::Output1 {
+    helper(input)
   }
 
-  type Output2 = String;
 
-  fn part_2(_input: &Self::Input) -> Self::Output2 {
-    unimplemented!("part_2")
+  type Output2 = i32;
+
+  fn part_2(input: &Self::Input) -> Self::Output2 {
+    let mut tmp = vec![];
+    for i in 1..input.len()-1 {
+      tmp.push(input[i-1]+input[i]+input[i+1])
+    }
+    helper(&tmp)
   }
 }
