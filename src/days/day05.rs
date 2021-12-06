21286105458c0e78907215689dfe712368f132cb
Rik van Toor <rik.van.toor@gmail.com>
2021-12-06 07:38:00 +0100

    Day 5

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -1,25 +1,88 @@
+use itertools::concat;
+use nom::bytes::complete::tag;
+use nom::character::complete::{i32, newline};
+use nom::multi::{separated_list0};
+use nom::sequence::tuple;
 use nom::IResult;
+use std::cmp::{max, min};
+use std::collections::HashMap;
 
 use crate::days::Day;
 
 pub struct Day05;
 
+fn parse_line(input: &str) -> IResult<&str, ((i32, i32), (i32, i32))> {
+  let (cont, (x1, _, y1, _, x2, _, y2)) =
+    tuple((i32, tag(","), i32, tag(" -> "), i32, tag(","), i32))(input)?;
+  Ok((cont, ((x1, y1), (x2, y2))))
+}
+
 impl Day for Day05 {
-  type Input = String;
+  type Input = Vec<((i32, i32), (i32, i32))>;
 
-  fn parse(_input: &str) -> IResult<&str, Self::Input> {
-    unimplemented!("parser")
+  fn parse(input: &str) -> IResult<&str, Self::Input> {
+    separated_list0(newline, parse_line)(input)
   }
 
-  type Output1 = String;
+  type Output1 = usize;
 
-  fn part_1(_input: &Self::Input) -> Self::Output1 {
-    unimplemented!("part_1")
+  fn part_1(input: &Self::Input) -> Self::Output1 {
+    let only_hor_ver: Self::Input = input
+      .into_iter()
+      .filter(|((x1, y1), (x2, y2))| x1 == x2 || y1 == y2)
+      .cloned()
+      .collect();
+
+    let points: Vec<(i32, i32)> = concat(only_hor_ver.into_iter().map(|((x1, y1), (x2, y2))| {
+      if y1 == y2 {
+        (min(x1, x2)..=max(x1, x2))
+          .map(|x| (x, y1))
+          .collect::<Vec<(i32, i32)>>()
+      } else {
+        (min(y1, y2)..=max(y1, y2))
+          .map(|y| (x1, y))
+          .collect::<Vec<(i32, i32)>>()
+      }
+    }));
+
+    let mut points_count: HashMap<(i32, i32), i32> = HashMap::new();
+    for p in points {
+      *points_count.entry(p).or_default() += 1;
+    }
+
+    let answer = points_count.iter().filter(|(_, x)| x > &&1).count();
+
+    answer
   }
 
-  type Output2 = String;
+  type Output2 = usize;
 
-  fn part_2(_input: &Self::Input) -> Self::Output2 {
-    unimplemented!("part_2")
+  fn part_2(input: &Self::Input) -> Self::Output2 {
+    let points: Vec<(i32, i32)> = concat(input.iter().cloned().map(|((x1, y1), (x2, y2))| {
+      if y1 == y2 {
+        (min(x1, x2)..=max(x1, x2))
+          .map(|x| (x, y1))
+          .collect::<Vec<(i32, i32)>>()
+      } else if x1 == x2 {
+        (min(y1, y2)..=max(y1, y2))
+          .map(|y| (x1, y))
+          .collect::<Vec<(i32, i32)>>()
+      } else {
+        let facx = if x2 > x1 { 1 } else { -1 };
+        let facy = if y2 > y1 { 1 } else { -1 };
+        (0..=(i32::abs(x2 - x1)))
+          .map(|d| (x1 + d * facx, y1 + d * facy))
+          .collect()
+      }
+    }));
+
+    let mut points_count: HashMap<(i32, i32), i32> = HashMap::new();
+    for p in points {
+      *points_count.entry(p).or_default() += 1;
+    }
+
+    let answer = points_count.iter().filter(|(_, x)| x > &&1).count();
+
+    answer
   }
 }
