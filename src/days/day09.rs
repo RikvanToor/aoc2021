688c1212c37c2af302cd7197c3c587dc7fb023c2
Rik van Toor <rik.van.toor@gmail.com>
2021-12-09 11:52:44 +0100

    Day 9

--- src/days/day09.rs
+++ src/days/day09.rs
@@ -1,25 +1,91 @@
+use nom::character::complete::{digit1, newline};
+use nom::multi::separated_list1;
 use nom::IResult;
+use std::collections::HashSet;
 
 use crate::days::Day;
 
 pub struct Day09;
 
+fn parse_line(input: &str) -> IResult<&str, Vec<u32>> {
+  let (cont, digits_str) = digit1(input)?;
+  let res = digits_str
+    .chars()
+    .map(|c| c.to_digit(10).unwrap())
+    .collect();
+  Ok((cont, res))
+}
+
+fn find_low_points(input: &Vec<Vec<u32>>) -> Vec<(usize, usize)> {
+  let height = input.len();
+  let width = input[0].len();
+  let mut res = vec![];
+  for y in 0..height {
+    for x in 0..width {
+      let val = input[y][x];
+      if (x == 0 || val < input[y][x - 1])
+        && (x == width - 1 || val < input[y][x + 1])
+        && (y == 0 || val < input[y - 1][x])
+        && (y == height - 1 || val < input[y + 1][x])
+      {
+        res.push((x, y));
+      }
+    }
+  }
+  res
+}
+
+fn expand_basin(
+  basin_points: &mut HashSet<(usize, usize)>,
+  input: &Vec<Vec<u32>>,
+  point: (usize, usize),
+) {
+  let (x, y) = point;
+  let height = input.len();
+  let width = input[0].len();
+  let adjacents = vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+  for p in adjacents {
+    let (x2, y2) = p;
+    if x2 < width && y2 < height && input[y2][x2] < 9 && !basin_points.contains(&p) {
+      basin_points.insert(p);
+      expand_basin(basin_points, input, p);
+    }
+  }
+}
+
 impl Day for Day09 {
-  type Input = String;
+  type Input = Vec<Vec<u32>>;
 
-  fn parse(_input: &str) -> IResult<&str, Self::Input> {
-    unimplemented!("parser")
+  fn parse(input: &str) -> IResult<&str, Self::Input> {
+    separated_list1(newline, parse_line)(input)
   }
 
-  type Output1 = String;
+  type Output1 = u32;
 
-  fn part_1(_input: &Self::Input) -> Self::Output1 {
-    unimplemented!("part_1")
+  fn part_1(input: &Self::Input) -> Self::Output1 {
+    let low_points = find_low_points(input);
+    low_points
+      .iter()
+      .map(|(x, y)| input[*y][*x] + 1)
+      .sum::<u32>()
   }
 
-  type Output2 = String;
+  type Output2 = u32;
 
-  fn part_2(_input: &Self::Input) -> Self::Output2 {
-    unimplemented!("part_2")
+  fn part_2(input: &Self::Input) -> Self::Output2 {
+    let low_points = find_low_points(input);
+
+    let mut basin_sizes: Vec<u32> = low_points
+      .iter()
+      .map(|p| {
+        let mut basin_points = HashSet::from([*p]);
+        expand_basin(&mut basin_points, input, *p);
+        basin_points.len() as u32
+      })
+      .collect();
+
+    basin_sizes.sort_by(|a, b| b.cmp(a));
+
+    basin_sizes[0..=2].iter().product::<u32>()
   }
 }
