f58be6b3c8270eebbd02b6e80e30442ce1024897
Rik van Toor <rik.van.toor@gmail.com>
2021-12-12 23:13:49 +0100

    More optimizations

--- src/days/day12.rs
+++ src/days/day12.rs
@@ -3,28 +3,23 @@ use nom::character::complete::{alpha1, newline};
 use nom::multi::separated_list0;
 use nom::sequence::tuple;
 use nom::IResult;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 use crate::days::Day;
 
 pub struct Day12;
 
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub enum Cave {
-  Other(u32),
-  Start,
-  End,
-}
+const START: u64 = 1;
+const END: u64 = 2;
 
 fn str_to_cave<'a>(
-  keys_map: &mut HashMap<&'a str, u32>,
-  max_value: &mut u32,
+  keys_map: &mut HashMap<&'a str, u64>,
+  max_value: &mut u64,
   input: &'a str,
-) -> Cave {
-  use Cave::*;
+) -> u64 {
   match input {
-    "start" => Start,
-    "end" => End,
+    "start" => START,
+    "end" => END,
     _ => {
       let is_small = input.chars().next().unwrap().is_lowercase();
       let key = match keys_map.get(input) {
@@ -36,7 +31,7 @@ fn str_to_cave<'a>(
           key
         }
       };
-      Other(key)
+      key
     }
   }
 }
@@ -46,18 +41,30 @@ fn parse_line(input: &str) -> IResult<&str, (&str, &str)> {
   Ok((cont, (a, b)))
 }
 
+fn make_map_key(c: u64, visited: u64, has_repeated_small: u64) -> u64 {
+  c << 25 | has_repeated_small << 24 | visited
+}
+
 fn run(
-  c: &Cave,
-  hm: &HashMap<Cave, HashSet<Cave>>,
-  visited: u32,
-  has_repeated_small: bool,
+  c: &u64,
+  hm: &HashMap<u64, Vec<u64>>,
+  res_map: &mut HashMap<u64, usize>,
+  visited: u64,
+  has_repeated_small: u64, // 1 as true, 0 as false
 ) -> usize {
-  use Cave::*;
   let nodes = hm.get(c).unwrap();
   let mut res = 0;
+  if let Some(x) = res_map.get(&make_map_key(*c, visited, has_repeated_small)) {
+    return *x;
+  }
   for c in nodes {
     match c {
-      Other(k) => {
+      1 => {} //start
+      2 => {
+        //end
+        res += 1;
+      }
+      k => {
         if k & 1 == 1 {
           // Remove small sign, just keep the ID
           let uk = k ^ 1;
@@ -65,46 +72,43 @@ fn run(
           if visited & uk != uk {
             //this node has not been visited
             let path = visited | uk;
-            let ress = run(c, hm, path, has_repeated_small);
+            let ress = run(c, hm, res_map, path, has_repeated_small);
             res += ress;
-          } else if !has_repeated_small {
+          } else if has_repeated_small == 0 {
             //This node has been visited, but we can repeat it
             let path = visited | uk;
-            let ress = run(c, hm, path, true);
+            let ress = run(c, hm, res_map, path, 1);
             res += ress;
           }
         } else {
           //cave is big
-          let ress = run(c, hm, visited.clone(), has_repeated_small);
+          let ress = run(c, hm, res_map, visited, has_repeated_small);
           res += ress;
         }
       }
-      End => {
-        res += 1;
-      }
-      Start => {}
     }
   }
+  res_map.insert(make_map_key(*c, visited, has_repeated_small), res);
   res
 }
 
 impl Day for Day12 {
-  type Input = HashMap<Cave, HashSet<Cave>>;
+  type Input = HashMap<u64, Vec<u64>>;
 
   fn parse(input: &str) -> IResult<&str, Self::Input> {
     let (cont, list) = separated_list0(newline, parse_line)(input)?;
-    let mut keys: HashMap<&str, u32> = HashMap::new();
-    let mut hm: HashMap<Cave, HashSet<Cave>> = HashMap::new();
+    let mut keys: HashMap<&str, u64> = HashMap::new();
+    let mut hm: HashMap<u64, Vec<u64>> = HashMap::new();
 
-    let mut max_value = 1;
+    let mut max_value = 4;
 
     for (s1, s2) in list {
       let c1 = str_to_cave(&mut keys, &mut max_value, s1);
       let c2 = str_to_cave(&mut keys, &mut max_value, s2);
-      let entry1 = hm.entry(c1.clone()).or_insert(HashSet::new());
-      entry1.insert(c2.clone());
-      let entry2 = hm.entry(c2.clone()).or_insert(HashSet::new());
-      entry2.insert(c1.clone());
+      let entry1 = hm.entry(c1).or_insert(vec![]);
+      entry1.push(c2);
+      let entry2 = hm.entry(c2).or_insert(vec![]);
+      entry2.push(c1);
     }
 
     Ok((cont, hm))
@@ -113,7 +117,8 @@ impl Day for Day12 {
   type Output1 = usize;
 
   fn part_1(input: &Self::Input) -> Self::Output1 {
-    let paths = run(&Cave::Start, input, 0, true);
+    let mut res_map = HashMap::new();
+    let paths = run(&START, input, &mut res_map, 0, 1);
 
     paths
   }
@@ -121,7 +126,8 @@ impl Day for Day12 {
   type Output2 = usize;
 
   fn part_2(input: &Self::Input) -> Self::Output2 {
-    let paths = run(&Cave::Start, input, 0, false);
+    let mut res_map = HashMap::new();
+    let paths = run(&START, input, &mut res_map, 0, 0);
 
     paths
   }
